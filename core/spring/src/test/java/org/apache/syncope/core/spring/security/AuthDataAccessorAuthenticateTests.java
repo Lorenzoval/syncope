9f0d3d77b18452a08d92b3de32f51ef1bb67f695
Lorenzo Valeriani <lorenzoval98@gmail.com>
2023-06-16 16:35:52 +0200

    Increase control flow coverage
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- core/spring/src/test/java/org/apache/syncope/core/spring/security/AuthDataAccessorAuthenticateTests.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/security/AuthDataAccessorAuthenticateTests.java
@@ -2,9 +2,13 @@ package org.apache.syncope.core.spring.security;
 
 import org.apache.commons.lang3.tuple.Triple;
 import org.apache.syncope.common.keymaster.client.api.ConfParamOps;
+import org.apache.syncope.common.lib.types.AnyTypeKind;
 import org.apache.syncope.common.lib.types.CipherAlgorithm;
+import org.apache.syncope.core.persistence.api.dao.AnySearchDAO;
 import org.apache.syncope.core.persistence.api.dao.RealmDAO;
 import org.apache.syncope.core.persistence.api.dao.UserDAO;
+import org.apache.syncope.core.persistence.api.dao.search.AttrCond;
+import org.apache.syncope.core.persistence.api.dao.search.SearchCond;
 import org.apache.syncope.core.persistence.api.entity.Realm;
 import org.apache.syncope.core.persistence.api.entity.user.User;
 import org.apache.syncope.core.spring.security.utils.EncryptorOracle;
@@ -16,6 +20,7 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.Mockito;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.Authentication;
 
 import javax.crypto.BadPaddingException;
@@ -23,6 +28,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.time.OffsetDateTime;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -35,25 +41,19 @@ public class AuthDataAccessorAuthenticateTests {
     private static final String INVALID_USERNAME = "invalidUsername";
     private static final String INVALID_PASSWORD = "invalidPassword";
     private static final String ACTIVE = "active";
-    private static final User user;
+    private static final String IOT_DEVICE = "iot_device";
 
-    static {
-        try {
-            user = createUser(USERNAME, PASSWORD, CipherAlgorithm.BCRYPT, PASSWORD);
-        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException |
-                 InvalidKeyException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static User createUser(String username, String password, CipherAlgorithm cipherAlgorithm, String secretKey)
+    public static User createUser(String username, String password, CipherAlgorithm cipherAlgorithm, String secretKey,
+                                  Boolean suspended, String status, int failedLogins)
             throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException,
             InvalidKeyException {
         User user = Mockito.mock(User.class);
         Mockito.when(user.getUsername()).thenReturn(username);
         Mockito.when(user.getPassword()).thenReturn(EncryptorOracle.encode(password, cipherAlgorithm, secretKey));
         Mockito.when(user.getCipherAlgorithm()).thenReturn(cipherAlgorithm);
-        Mockito.when(user.getStatus()).thenReturn(ACTIVE);
+        Mockito.when(user.getStatus()).thenReturn(status);
+        Mockito.when(user.isSuspended()).thenReturn(suspended);
+        Mockito.when(user.getFailedLogins()).thenReturn(failedLogins);
         return user;
     }
 
@@ -64,14 +64,25 @@ public class AuthDataAccessorAuthenticateTests {
         return userDAO;
     }
 
-    public static ConfParamOps createConfParamOps(String domain) {
+    public static AnySearchDAO createAnySearchDAO(List<User> users, String attribute) {
+        AnySearchDAO anySearchDAO = Mockito.mock(AnySearchDAO.class);
+        for (User user : users) {
+            AttrCond attrCond = new AttrCond(AttrCond.Type.EQ);
+            attrCond.setSchema(attribute);
+            attrCond.setExpression(user.getUsername());
+            Mockito.when(anySearchDAO.search(SearchCond.getLeaf(attrCond), AnyTypeKind.USER)).thenReturn(List.of(user));
+        }
+        return anySearchDAO;
+    }
+
+    public static ConfParamOps createConfParamOps(String domain, String attribute, boolean logLastLoginDate) {
         ConfParamOps confParamOps = Mockito.mock(ConfParamOps.class);
         Mockito.when(confParamOps.get(domain, "authentication.attributes", new String[]{"username"},
-                String[].class)).thenReturn(new String[]{USERNAME});
+                String[].class)).thenReturn(new String[]{attribute});
         Mockito.when(confParamOps.get(domain, "authentication.statuses", new String[]{}, String[].class))
                 .thenReturn(new String[]{ACTIVE});
         Mockito.when(confParamOps.get(domain, "log.lastlogindate", true, Boolean.class))
-                .thenReturn(false);
+                .thenReturn(logLastLoginDate);
         return confParamOps;
     }
 
@@ -99,10 +110,21 @@ public class AuthDataAccessorAuthenticateTests {
             case INVALID_PASSWORD -> authentication = createAuthentication(USERNAME, INVALID_PASSWORD, DOMAIN);
             case EMPTY_PASSWORD -> authentication = createAuthentication(USERNAME, "", DOMAIN);
             case NULL_PASSWORD -> authentication = createAuthentication(USERNAME, null, DOMAIN);
-            // Case valid
-            default -> authentication = createAuthentication(USERNAME, PASSWORD, DOMAIN);
+            default -> authentication = createAuthentication(USERNAME, PASSWORD, DOMAIN); // Case VALID
         }
         return authentication;
+    }
+
+    public static Triple<User, Boolean, String> expectedResultToTriple(ExpectedResult expectedResult, User user) {
+        Triple<User, Boolean, String> triple = null;
+        switch (expectedResult) {
+            case NULL_NULL_NULL -> triple = Triple.of(null, null, null);
+            case USER_TRUE_NULL -> triple = Triple.of(user, true, null);
+            case USER_FALSE_NULL -> triple = Triple.of(user, false, null);
+            default -> {
+            } // Case NULL
+        }
+        return triple;
     }
 
     @Ignore
@@ -115,41 +137,68 @@ public class AuthDataAccessorAuthenticateTests {
         NULL_PASSWORD
     }
 
+    @Ignore
+    public enum ExpectedResult {
+        // To be expanded as needed
+        NULL,
+        NULL_NULL_NULL,
+        USER_FALSE_NULL,
+        USER_TRUE_NULL
+    }
+
     @RunWith(Parameterized.class)
     public static class AuthDataAccessorAuthenticateTest {
-        private final Triple<User, Boolean, String> expected;
+        private final ExpectedResult expectedResult;
         private final Class<Throwable> exceptionClass;
         private final String domain;
         private final Authentication authentication;
+        private final String attribute;
+        private Triple<User, Boolean, String> expected;
         private AuthDataAccessor sut;
 
-        public AuthDataAccessorAuthenticateTest(Triple<User, Boolean, String> expected, Class<Throwable> exceptionClass, String domain,
-                                                AuthenticationType authenticationType) {
-            this.expected = expected;
+        public AuthDataAccessorAuthenticateTest(ExpectedResult expectedResult, Class<Throwable> exceptionClass, String domain,
+                                                AuthenticationType authenticationType, String attribute) {
+            this.expectedResult = expectedResult;
             this.exceptionClass = exceptionClass;
             this.domain = domain;
             this.authentication = createAuthentication(authenticationType);
+            this.attribute = attribute;
         }
 
         @Parameterized.Parameters
         public static Collection<Object[]> getParameters() {
             return Arrays.asList(new Object[][]{
-                    {null, NullPointerException.class, null, AuthenticationType.VALID},
-                    {null, NullPointerException.class, DOMAIN, AuthenticationType.NULL},
-                    {Triple.of(user, true, null), null, DOMAIN, AuthenticationType.VALID},
-                    {Triple.of(null, null, null), null, DOMAIN, AuthenticationType.INVALID_USERNAME},
-                    {Triple.of(user, false, null), null, DOMAIN, AuthenticationType.INVALID_PASSWORD},
-                    {Triple.of(user, false, null), null, DOMAIN, AuthenticationType.EMPTY_PASSWORD},
-                    {null, NullPointerException.class, DOMAIN, AuthenticationType.NULL_PASSWORD}
+                    {ExpectedResult.NULL, NullPointerException.class, null, AuthenticationType.VALID, USERNAME},
+                    {ExpectedResult.NULL, NullPointerException.class, DOMAIN, AuthenticationType.NULL, USERNAME},
+                    {ExpectedResult.USER_TRUE_NULL, null, DOMAIN, AuthenticationType.VALID, USERNAME},
+                    {ExpectedResult.NULL_NULL_NULL, null, DOMAIN, AuthenticationType.INVALID_USERNAME, USERNAME},
+                    {ExpectedResult.USER_FALSE_NULL, null, DOMAIN, AuthenticationType.INVALID_PASSWORD, USERNAME},
+                    {ExpectedResult.USER_FALSE_NULL, null, DOMAIN, AuthenticationType.EMPTY_PASSWORD, USERNAME},
+                    {ExpectedResult.NULL, NullPointerException.class, DOMAIN, AuthenticationType.NULL_PASSWORD,
+                            USERNAME},
+                    {ExpectedResult.NULL, NullPointerException.class, null, AuthenticationType.VALID, IOT_DEVICE},
+                    {ExpectedResult.NULL, NullPointerException.class, DOMAIN, AuthenticationType.NULL, IOT_DEVICE},
+                    {ExpectedResult.USER_TRUE_NULL, null, DOMAIN, AuthenticationType.VALID, IOT_DEVICE},
+                    {ExpectedResult.NULL_NULL_NULL, null, DOMAIN, AuthenticationType.INVALID_USERNAME, IOT_DEVICE},
+                    {ExpectedResult.USER_FALSE_NULL, null, DOMAIN, AuthenticationType.INVALID_PASSWORD, IOT_DEVICE},
+                    {ExpectedResult.USER_FALSE_NULL, null, DOMAIN, AuthenticationType.EMPTY_PASSWORD, IOT_DEVICE},
+                    {ExpectedResult.NULL, NullPointerException.class, DOMAIN, AuthenticationType.NULL_PASSWORD,
+                            IOT_DEVICE},
             });
         }
 
         @Before
-        public void init() {
+        public void init() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException,
+                BadPaddingException, InvalidKeyException {
+            User user = createUser(USERNAME, PASSWORD, CipherAlgorithm.BCRYPT, PASSWORD, false, ACTIVE, 0);
+            this.expected = expectedResultToTriple(this.expectedResult, user);
             UserDAO userDAO = createUserDAO(List.of(user));
+            AnySearchDAO anySearchDAO = null;
+            if (!USERNAME.equals(this.attribute))
+                anySearchDAO = createAnySearchDAO(List.of(user), this.attribute);
             RealmDAO realmDAO = createRealmDAO();
-            ConfParamOps confParamOps = createConfParamOps(DOMAIN);
-            this.sut = new AuthDataAccessor(new SecurityProperties(), realmDAO, userDAO, null, null,
+            ConfParamOps confParamOps = createConfParamOps(DOMAIN, this.attribute, false);
+            this.sut = new AuthDataAccessor(new SecurityProperties(), realmDAO, userDAO, null, anySearchDAO,
                     null, confParamOps, null, null, null,
                     null, null, null);
         }
@@ -166,6 +215,84 @@ public class AuthDataAccessorAuthenticateTests {
                 }
                 Assert.assertTrue(this.exceptionClass.isInstance(e));
             }
+        }
+
+    }
+
+    public static class AuthDataAccessorAuthenticateMiscTests {
+
+        @Test
+        public void testSuspended() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException,
+                BadPaddingException, InvalidKeyException {
+            User user = createUser(USERNAME, PASSWORD, CipherAlgorithm.BCRYPT, PASSWORD, true, ACTIVE, 0);
+            UserDAO userDAO = createUserDAO(List.of(user));
+            RealmDAO realmDAO = createRealmDAO();
+            ConfParamOps confParamOps = createConfParamOps(DOMAIN, USERNAME, false);
+            Authentication authentication = createAuthentication(AuthenticationType.VALID);
+            AuthDataAccessor sut = new AuthDataAccessor(new SecurityProperties(), realmDAO, userDAO, null,
+                    null, null, confParamOps, null, null,
+                    null, null, null, null);
+            Assert.assertThrows(DisabledException.class, () -> sut.authenticate(DOMAIN, authentication));
+        }
+
+        @Test
+        public void testNullSuspended() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException,
+                BadPaddingException, InvalidKeyException {
+            User user = createUser(USERNAME, PASSWORD, CipherAlgorithm.BCRYPT, PASSWORD, null, ACTIVE, 0);
+            UserDAO userDAO = createUserDAO(List.of(user));
+            RealmDAO realmDAO = createRealmDAO();
+            ConfParamOps confParamOps = createConfParamOps(DOMAIN, USERNAME, false);
+            Authentication authentication = createAuthentication(AuthenticationType.VALID);
+            AuthDataAccessor sut = new AuthDataAccessor(new SecurityProperties(), realmDAO, userDAO, null,
+                    null, null, confParamOps, null, null,
+                    null, null, null, null);
+            Triple<User, Boolean, String> result = sut.authenticate(DOMAIN, authentication);
+            Assert.assertEquals(result, Triple.of(user, true, null));
+        }
+
+        @Test
+        public void testUndefinedStatus() throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException,
+                BadPaddingException, InvalidKeyException {
+            User user = createUser(USERNAME, PASSWORD, CipherAlgorithm.BCRYPT, PASSWORD, false, USERNAME, 0);
+            UserDAO userDAO = createUserDAO(List.of(user));
+            RealmDAO realmDAO = createRealmDAO();
+            ConfParamOps confParamOps = createConfParamOps(DOMAIN, USERNAME, false);
+            Authentication authentication = createAuthentication(AuthenticationType.VALID);
+            AuthDataAccessor sut = new AuthDataAccessor(new SecurityProperties(), realmDAO, userDAO, null,
+                    null, null, confParamOps, null, null,
+                    null, null, null, null);
+            Assert.assertThrows(DisabledException.class, () -> sut.authenticate(DOMAIN, authentication));
+        }
+
+        @Test
+        public void testLogLastLoginDate() throws NoSuchPaddingException, IllegalBlockSizeException,
+                NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+            User user = createUser(USERNAME, PASSWORD, CipherAlgorithm.BCRYPT, PASSWORD, false, ACTIVE, 0);
+            UserDAO userDAO = createUserDAO(List.of(user));
+            RealmDAO realmDAO = createRealmDAO();
+            ConfParamOps confParamOps = createConfParamOps(DOMAIN, USERNAME, true);
+            Authentication authentication = createAuthentication(AuthenticationType.VALID);
+            AuthDataAccessor sut = new AuthDataAccessor(new SecurityProperties(), realmDAO, userDAO, null,
+                    null, null, confParamOps, null, null,
+                    null, null, null, null);
+            sut.authenticate(DOMAIN, authentication);
+            Mockito.verify(user, Mockito.times(1))
+                    .setLastLoginDate(Mockito.any(OffsetDateTime.class));
+        }
+
+        @Test
+        public void testFailedLogins() throws NoSuchPaddingException, IllegalBlockSizeException,
+                NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+            User user = createUser(USERNAME, PASSWORD, CipherAlgorithm.BCRYPT, PASSWORD, false, ACTIVE, 1);
+            UserDAO userDAO = createUserDAO(List.of(user));
+            RealmDAO realmDAO = createRealmDAO();
+            ConfParamOps confParamOps = createConfParamOps(DOMAIN, USERNAME, false);
+            Authentication authentication = createAuthentication(AuthenticationType.VALID);
+            AuthDataAccessor sut = new AuthDataAccessor(new SecurityProperties(), realmDAO, userDAO, null,
+                    null, null, confParamOps, null, null,
+                    null, null, null, null);
+            sut.authenticate(DOMAIN, authentication);
+            Mockito.verify(user, Mockito.times(1)).setFailedLogins(0);
         }
 
     }
